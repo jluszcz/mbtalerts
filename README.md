6fa490be589667e0bdc6b941599f738933b82c58
Jacob Luszcz <jacob.luszcz@gmail.com>
2025-03-30 10:13:40 -0400

    Set up libraries properly

--- README.md
+++ README.md
@@ -4,10 +4,6 @@
 
 _Requires `openapi-generator` to be installed_
 
-**TODO**: `/Users/jacob/Documents/Programs/mbta-api/apps/api_web/priv/static/swagger.json` in `openapi.json` should be
-replaced with `https://api-v3.mbta.com/docs/swagger/swagger.json` once https://github.com/mbta/api/pull/871 is
-merged.
-
 ```bash
 openapi-generator generate -c src/mbta-client/openapi.json
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,48 @@
-mod mbtalerts;
+use clap::{Arg, ArgAction, Command};
+use log::debug;
+use mbtalerts::set_up_logger;
+
+#[derive(Debug)]
+struct Args {
+    verbose: bool,
+    _use_cache: bool,
+}
+
+fn parse_args() -> Args {
+    let matches = Command::new("mbtalerts")
+        .version("0.1")
+        .author("Jacob Luszcz")
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .action(ArgAction::SetTrue)
+                .help("Verbose mode. Outputs DEBUG and higher log messages."),
+        )
+        .arg(
+            Arg::new("use-cache")
+                .short('c')
+                .long("cache")
+                .action(ArgAction::SetTrue)
+                .help("Use cached values, if present, rather than querying remote services."),
+        )
+        .get_matches();
+
+    let verbose = matches.get_flag("verbose");
+
+    let use_cache = matches.get_flag("use-cache");
+
+    Args {
+        verbose,
+        _use_cache: use_cache,
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world");
+    let args = parse_args();
+    set_up_logger(module_path!(), args.verbose)?;
+    debug!("{:?}", args);
+
     Ok(())
 }
