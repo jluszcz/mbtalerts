935bc0c6f4326ac49e0ad2842ed0665998866169
Jacob Luszcz <jacob.luszcz@gmail.com>
2026-02-21 08:09:45 -0500

    Set up Lambda/Terraform

--- src/lambda.rs
+++ src/lambda.rs
@@ -1,4 +1,4 @@
-use jluszcz_rust_utils::{Verbosity, set_up_logger};
+use jluszcz_rust_utils::{Verbosity, set_up_logger, lambda};
 use lambda_runtime::{LambdaEvent, service_fn};
 use mbtalerts::APP_NAME;
 use mbtalerts::calendar::{CalendarClient, sync_alerts};
@@ -6,17 +6,17 @@ use serde_json::{Value, json};
 
 #[tokio::main]
 async fn main() -> Result<(), lambda_runtime::Error> {
-    set_up_logger(APP_NAME, module_path!(), Verbosity::Debug)?;
-    lambda_runtime::run(service_fn(handler)).await
+    lambda::init(APP_NAME, module_path!(), false).await?;
+    let func = service_fn(handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
 }
 
 async fn handler(_event: LambdaEvent<Value>) -> Result<Value, lambda_runtime::Error> {
     let alerts = mbtalerts::alerts(false).await?;
+
     let calendar = CalendarClient::from_env().await?;
     sync_alerts(&alerts, &calendar).await?;
 
-    Ok(json!({
-        "status": "ok",
-        "alerts_processed": alerts.data.len(),
-    }))
+    Ok(json!({}))
 }
